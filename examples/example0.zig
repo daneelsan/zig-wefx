280123c2490a7c61d5795aae1fdd598072457d90
Daniel Sanchez <dansando@protonmail.com>
2022-05-04 21:21:17 -0500

    Now the init function should return a pointer to a WEFX instance.

--- examples/example0.zig
+++ examples/example0.zig
@@ -12,48 +12,42 @@ var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
 var rng = std.rand.DefaultPrng.init(42);
 const random = rng.random();
 
-export fn init() u32 {
-    wefx.instance = wefx.WEFX.init(arena.allocator());
-    wefx.instance.open(width, height) catch {
-        // A non-zero return value is an error, which should be handled in JS.
-        return 1;
-    };
+var w: wefx.WEFX = undefined;
 
-    // wefx_event_queue *queue = wefx_open_events();
-    // if (queue == NULL)
-    //     return 1;
-    // enable mouse and keyboard events
-    //wefx.instance.openEvents();
+export fn init() ?*wefx.WEFX {
+    w = wefx.WEFX.init(arena.allocator());
 
-    wefx.instance.clearColor(0x00, 0x00, 0x00);
-    wefx.instance.color(0xFF, 0xFF, 0xFF);
-    wefx.instance.clear();
+    w.open(width, height) catch return null;
 
-    return 0;
+    w.clearColor(0x00, 0x00, 0x00);
+    w.color(0xFF, 0xFF, 0xFF);
+    w.clear();
+
+    return &w;
 }
 
 fn draw() void {
-    wefx.instance.clear();
-    wefx.instance.color(0xEE, 0xDD, 0xCC);
-    wefx.instance.color(random.int(u8), random.int(u8), random.int(u8));
+    w.clear();
+    w.color(0xEE, 0xDD, 0xCC);
+    w.color(random.int(u8), random.int(u8), random.int(u8));
 
     // W
-    wefx.instance.line(15, 15, 20, 35);
-    wefx.instance.line(20, 35, 22, 25);
-    wefx.instance.line(23, 25, 25, 35);
-    wefx.instance.line(25, 35, 30, 15);
+    w.line(15, 15, 20, 35);
+    w.line(20, 35, 22, 25);
+    w.line(23, 25, 25, 35);
+    w.line(25, 35, 30, 15);
     // E
-    wefx.instance.line(50, 15, 35, 15);
-    wefx.instance.line(45, 25, 35, 25);
-    wefx.instance.line(50, 35, 35, 35);
-    wefx.instance.line(35, 15, 35, 35);
+    w.line(50, 15, 35, 15);
+    w.line(45, 25, 35, 25);
+    w.line(50, 35, 35, 35);
+    w.line(35, 15, 35, 35);
     // F
-    wefx.instance.line(70, 15, 55, 15);
-    wefx.instance.line(65, 25, 55, 25);
-    wefx.instance.line(55, 15, 55, 35);
+    w.line(70, 15, 55, 15);
+    w.line(65, 25, 55, 25);
+    w.line(55, 15, 55, 35);
     // X
-    wefx.instance.line(75, 15, 90, 35);
-    wefx.instance.line(90, 15, 75, 35);
+    w.line(75, 15, 90, 35);
+    w.line(90, 15, 75, 35);
 }
 
 export fn main_loop(time: f32) void {
